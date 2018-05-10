7064aa5dd8c874a2af5c3331a2f001a8917cbf9e
lbl <596038980@qq.com>
2018-05-10 16:03:44 +0800

    去除@RequestBody注解

--- area-svr/src/main/java/rebue/area/ctrl/AreAreaCtrl.java
+++ area-svr/src/main/java/rebue/area/ctrl/AreAreaCtrl.java
@@ -49,7 +49,7 @@ public class AreAreaCtrl {
      * @date 2018年4月6日 下午3:33:28
      */
     @GetMapping("/are/area/lowerareaall")
-    List<AreAreaMo> selectLowerAreaAll(@RequestBody AreAreaMo qo) {
+    List<AreAreaMo> selectLowerAreaAll(AreAreaMo qo) {
     	_log.info("开始获取所有下级区域信息的参数为：{}" + qo.getAreaCode());
     	return svc.selectLowerAreaAll(qo);
     }
@@ -63,7 +63,7 @@ public class AreAreaCtrl {
      * @date 2018年4月6日 下午3:36:04
      */
     @GetMapping("/are/area/countyall")
-    List<AreAreaMo> selectCountyAllByCityCode(@RequestBody AreAreaMo qo) {
+    List<AreAreaMo> selectCountyAllByCityCode(AreAreaMo qo) {
     	_log.info("开始获取所有直辖镇信息的参数为：{}" + qo.getAreaCode());
     	return svc.selectCountyAllByCityCode(qo);
     }
