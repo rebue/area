b013de70d4059691752f760e5f9a774240cb05f6
lbl <596038980@qq.com>
2018-05-18 16:31:37 +0800

    去除toString

--- area-svr/src/main/java/rebue/area/ctrl/AreAreaCtrl.java
+++ area-svr/src/main/java/rebue/area/ctrl/AreAreaCtrl.java
@@ -6,7 +6,6 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rebue.area.mo.AreAreaMo;
